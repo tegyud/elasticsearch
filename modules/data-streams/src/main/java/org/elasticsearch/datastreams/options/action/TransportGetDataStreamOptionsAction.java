9842fc86babc0f1f84ff4d9ef7bd0d0e9bccc40c
Niels Bauman <nielsbauman@gmail.com>
2025-02-15 18:10:59 +1000

    Run `TransportGetDataStreamOptionsAction` on local node
    This action solely needs the cluster state, it can run on any node.
    Additionally, it needs to be cancellable to avoid doing unnecessary work
    after a client failure or timeout.
    
    Relates #101805

--- modules/data-streams/src/main/java/org/elasticsearch/datastreams/options/action/TransportGetDataStreamOptionsAction.java
+++ modules/data-streams/src/main/java/org/elasticsearch/datastreams/options/action/TransportGetDataStreamOptionsAction.java
@@ -11,7 +11,8 @@ package org.elasticsearch.datastreams.options.action;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.datastreams.DataStreamsActionUtil;
 import org.elasticsearch.action.support.ActionFilters;
-import org.elasticsearch.action.support.master.TransportMasterNodeReadAction;
+import org.elasticsearch.action.support.ChannelActionListener;
+import org.elasticsearch.action.support.local.TransportLocalClusterStateAction;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
@@ -19,6 +20,7 @@ import org.elasticsearch.cluster.metadata.DataStream;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.core.UpdateForV10;
 import org.elasticsearch.indices.SystemIndices;
 import org.elasticsearch.injection.guice.Inject;
 import org.elasticsearch.tasks.Task;
@@ -34,13 +36,20 @@ import java.util.Objects;
  * Collects the data streams from the cluster state and then returns for each data stream its name and its
  * data stream options. Currently, data stream options include only the failure store configuration.
  */
-public class TransportGetDataStreamOptionsAction extends TransportMasterNodeReadAction<
+public class TransportGetDataStreamOptionsAction extends TransportLocalClusterStateAction<
     GetDataStreamOptionsAction.Request,
     GetDataStreamOptionsAction.Response> {
 
     private final IndexNameExpressionResolver indexNameExpressionResolver;
     private final SystemIndices systemIndices;
+    private final ThreadPool threadPool;
 
+    /**
+     * NB prior to 9.0 this was a TransportMasterNodeReadAction so for BwC it must be registered with the TransportService until
+     * we no longer need to support calling this action remotely.
+     */
+    @UpdateForV10(owner = UpdateForV10.Owner.DATA_MANAGEMENT)
+    @SuppressWarnings("this-escape")
     @Inject
     public TransportGetDataStreamOptionsAction(
         TransportService transportService,
@@ -52,20 +61,27 @@ public class TransportGetDataStreamOptionsAction extends TransportMasterNodeRead
     ) {
         super(
             GetDataStreamOptionsAction.INSTANCE.name(),
-            transportService,
-            clusterService,
-            threadPool,
             actionFilters,
-            GetDataStreamOptionsAction.Request::new,
-            GetDataStreamOptionsAction.Response::new,
+            transportService.getTaskManager(),
+            clusterService,
             EsExecutors.DIRECT_EXECUTOR_SERVICE
         );
         this.indexNameExpressionResolver = indexNameExpressionResolver;
         this.systemIndices = systemIndices;
+        this.threadPool = threadPool;
+
+        transportService.registerRequestHandler(
+            actionName,
+            executor,
+            false,
+            true,
+            GetDataStreamOptionsAction.Request::new,
+            (request, channel, task) -> executeDirect(task, request, new ChannelActionListener<>(channel))
+        );
     }
 
     @Override
-    protected void masterOperation(
+    protected void localClusterStateOperation(
         Task task,
         GetDataStreamOptionsAction.Request request,
         ClusterState state,
